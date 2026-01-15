e1f7db60d188c4f080e147f0280ff5d5992fb426
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-01-15 19:24:21 -0300

    feat: configure PostgreSQL connection

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+
+import os
+
 class Config:
     SECRET_KEY = "dev"
     DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://exactum_admin:root@localhost:5432/exactum_db"
+    )
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-secret")
