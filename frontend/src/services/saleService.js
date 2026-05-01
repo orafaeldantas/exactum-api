685a3bb40bce1d014fab68116573c58c7c46a689
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-05-01 12:39:36 -0300

    feat(frontend): add the sales service to retrieve sales data from the API

---
+++ frontend/src/services/saleService.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { apiFetch } from "./api";
+
+export function getSales() { 
+  const [sales, setSales] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function loadSales() {
+    try {
+      setLoading(true);
+      const response = await apiFetch("/sales");
+      const data = await response.json();
+      setUsers(data);
+    } catch (err) {
+      console.error("Erro ao carregar:", err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
+  return { sales, loadSales, loading };
+}
