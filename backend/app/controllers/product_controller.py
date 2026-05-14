6090da30eea300eac30914181beba93f5ddcf79a
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-05-13 22:57:07 -0300

    refactor(backend): change products logic to mvc

---
+++ backend/app/controllers/product_controller.py
@@ -0,0 +1,30 @@
+from flask import g
+from services.product_service import ProductService
+
+
+class ProductController:
+
+    @staticmethod
+    def create_product(data):
+
+        return ProductService.create_tenant(data)
+    
+    @staticmethod
+    def list_all_products():
+
+        return ProductService.list_all_products(g.tenant_id)
+    
+    @staticmethod
+    def get_product(product_id):
+
+        return ProductService.get_product(product_id)
+    
+    @staticmethod
+    def update_product(data, product_id):
+
+        return ProductService.update_product(data, product_id)
+    
+    @staticmethod
+    def delete_product(product_id):
+
+        return ProductService.delete_product(product_id)
