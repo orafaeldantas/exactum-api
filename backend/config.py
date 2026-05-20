c5d0a84516d2bd1c43bd00454aa0bec3f348fd89
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-05-19 22:08:27 -0300

    feat(fullstack): add ngix

--- backend/config.py
+++ backend/config.py
@@ -1,23 +1,31 @@
 import os
 from datetime import timedelta
 
+
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    DEBUG = os.getenv("DEBUG")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    DEBUG = os.getenv("DEBUG", "False") == "True"
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES")))
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(seconds=int(os.getenv("JWT_REFRESH_TOKEN_EXPIRES")))
+
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        seconds=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 900))
+    )
+
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(
+        seconds=int(os.getenv("JWT_REFRESH_TOKEN_EXPIRES", 2592000))
+    )
+
     JWT_TOKEN_LOCATION = ["headers"]
     JWT_HEADER_NAME = "Authorization"
     JWT_HEADER_TYPE = "Bearer"
 
     JWT_COOKIE_CSRF_PROTECT = False
 
-    # === SWAGGER ===
+    # Swagger
     API_TITLE = "Exactum API"
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0.3"
@@ -28,16 +36,16 @@ class Config:
     )
 
     API_SPEC_OPTIONS = {
-    "components": {
-        "securitySchemes": {
-            "BearerAuth": {
-                "type": "http",
-                "scheme": "bearer",
-                "bearerFormat": "JWT"
+        "components": {
+            "securitySchemes": {
+                "BearerAuth": {
+                    "type": "http",
+                    "scheme": "bearer",
+                    "bearerFormat": "JWT"
+                }
             }
         }
     }
-}
 
 
 
