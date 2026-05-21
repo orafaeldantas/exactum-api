030b929614e1b99f05dd747403d22dd3a80bf0cc
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-05-21 02:24:15 -0300

    fix(infra): change root command in nginx file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,10 +10,12 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:5000",
-        changeOrigin: true,
+  
+        rewrite: (path) =>
+          path.replace(/^\/api/, "")
       }
     }
-  },
+  }
 
 })
 
