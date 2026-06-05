e2e9201193344740ad10cbb4006f00d985ffa9e0
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-06-05 16:23:23 -0300

    ci(repo): add pytest, mypy and pip-audit jobs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-      'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'format', 'perf', 'chore', 'docs']],
+      'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'format', 'perf', 'chore', 'docs', 'ci', `style`]],
       'scope-enum': [2, 'always', ['ui', 'frontend', 'api', 'backend', 'db', 'infra', 'repo']],
     },
   };
