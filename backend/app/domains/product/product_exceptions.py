2ad66354c46b33845b7623c4afbc254b43375deb
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-15 20:08:48 -0300

    feat(api): add soft delete for products

--- backend/app/domains/product/product_exceptions.py
+++ backend/app/domains/product/product_exceptions.py
@@ -5,3 +5,9 @@ class ProductNotFound(AppException):
     status_code = 404
 
     message = "Product not found"
+
+
+class ExistingProductField(AppException):
+    status_code = 409
+
+    message = "Some fields are already registered in another product"
