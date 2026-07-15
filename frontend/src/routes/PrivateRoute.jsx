f7941fec21c6d73b30a67263577c912fc810cc53
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-14 23:45:59 -0300

    feat(frontend): add logic to manage public and private routes

---
+++ frontend/src/routes/PrivateRoute.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react"
+import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext"
+import GlobalLoader from "../components/Loader/GlobalLoader";
+import { useLocation } from "react-router-dom";
+import { SuperAdminContext } from "../context/SuperAdminContext";
+
+
+
+export default function PrivateRoute({ children, requiredRole }) {
+
+  const { user, loading, impersonateMode, permissions } = useContext(AuthContext)
+  const { superAdmin } = useContext(SuperAdminContext)
+  const location = useLocation();
+
+  const blockWhenLoggedIn= [
+    "PageNotAuth"
+  ]
+
+  if (loading) {
+    return <GlobalLoader message="Carregando..." />;
+  }
+
+  if (!user) {
+    return <Navigate to="/" replace />
+  }
+
+  if (user?.password_reset === true && location.pathname !== "/reset-password" 
+      && !superAdmin && impersonateMode !== true) {
+    return <Navigate to="/reset-password" replace />;
+  }
+
+  if (requiredRole && !permissions.includes(requiredRole)) { 
+
+    if (superAdmin) return <Navigate to="/platform/dashboard" replace />
+
+    return <Navigate to="/dashboard" replace />
+  }
+
+
+  return children ? children : <Outlet />;
+}
