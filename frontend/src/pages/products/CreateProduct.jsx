c64d66621088598ce488c0b71f59c8c4ac750554
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-15 20:22:58 -0300

    feat(frontend): add error check to display message to the user

--- frontend/src/pages/products/CreateProduct.jsx
+++ frontend/src/pages/products/CreateProduct.jsx
@@ -1,31 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import { apiFetch } from "../../services/api";
 import ProductForm from "../../features/product-form/ProductForm";
+import toast from "react-hot-toast";
 
 export default function CreateProduct() {
-
   const navigate = useNavigate();
 
   async function handleCreate(data) {
-    const response = await apiFetch("/products", {
-      method: "POST",
-      body: JSON.stringify(data)
-    });
+    try {
+      const response = await apiFetch("/products", {
+        method: "POST",
+        body: JSON.stringify(data)
+      });
 
-    if (response.ok) {
-      navigate("/products");
+      if (response && response.ok) {
+        toast.success("Produto criado com sucesso!");
+        navigate("/products");
+      }
+   
+    } catch (error) {
+
+      const status = error.status || error.response?.status;
+
+      console.log(status)
+
+      if (status === 409) {         
+        toast.error("Erro ao salvar: Este SKU já está cadastrado.");
+      } else if (status === 400) {
+        toast.error("Dados inválidos. Verifique o formulário.");
+      } else {
+        toast.error("Erro interno no servidor (500). Tente mais tarde.");
+      }
     }
-
   }
 
   return (
     <div>
-      
       <ProductForm
         onSubmit={handleCreate}
         submitText="Criar produto"
       />
-
     </div>
   );
 }
