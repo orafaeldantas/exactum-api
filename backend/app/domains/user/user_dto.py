4937393816dae4616f668a4402966c159ca25613
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-16 19:13:45 -0300

    feat(api): add mapper and dto to users

---
+++ backend/app/domains/user/user_dto.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+
+@dataclass(slots=True, frozen=True)
+class GetUserDTO:
+    uuid: UUID
+    username: str | None
+    is_active: bool | None
+    email: str | None
+    role: str | None
