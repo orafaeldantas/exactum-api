5bc391fd2ad376340910d156cb5dbcecf6043ebf
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-21 20:36:07 -0300

    feat(backend): add a method to generate the permission key for writing to redis

--- backend/app/core/cache/cache_keys.py
+++ backend/app/core/cache/cache_keys.py
@@ -1,5 +1,10 @@
 class CacheKeys:
     @staticmethod
-    def refresh_token(jti: str, user_id: int):
+    def refresh_token(jti: str, tenant_id: int, user_id: int):
 
-        return f"user:{user_id}:refresh:{jti}"
+        return f"tenant:{tenant_id}:user:{user_id}:refresh:{jti}"
+
+    @staticmethod
+    def permissions(tenant_id: int, user_id: int):
+
+        return f"tenant:{tenant_id}:user:{user_id}:permissions"
