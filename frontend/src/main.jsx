216d72bc7dc414e6beeeb8376495222f6a3c883c
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-22 00:42:06 -0300

    style(frontend): reformat code with Prettier

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import { UserProvider } from "./context/UserContext";
-import { TenantProvider } from "./context/TenantContext";
 import { SuperAdminProvider } from "./context/SuperAdminContext";
-import './styles/index.css';
+import { TenantProvider } from "./context/TenantContext";
+import { UserProvider } from "./context/UserContext";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SuperAdminProvider>   
+      <SuperAdminProvider>
         <UserProvider>
           <TenantProvider>
             <AuthProvider>
