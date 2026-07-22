4cb259b223eba55212ec8fdd33bc66bf9ccde9a1
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-22 00:21:30 -0300

    feat(backend): add keys to black list

--- backend/app/core/cache/cache_keys.py
+++ backend/app/core/cache/cache_keys.py
@@ -8,3 +8,11 @@ class CacheKeys:
     def permissions(tenant_id: int, user_id: int):
 
         return f"tenant:{tenant_id}:user:{user_id}:permissions"
+
+    @staticmethod
+    def black_list_user(tenant_id: int, user_id: int):
+        return f"black_list:tenant:{tenant_id}:user:{user_id}"
+
+    @staticmethod
+    def black_list_tenant(tenant_id: int):
+        return f"black_list:tenant:{tenant_id}:*"
