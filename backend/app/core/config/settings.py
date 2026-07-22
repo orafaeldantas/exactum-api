ef227d9a0ab71a1eed58bd3cdb557e05e521cd26
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-07-22 00:24:33 -0300

    feat(backend): add black_list_ttl in settings

--- backend/app/core/config/settings.py
+++ backend/app/core/config/settings.py
@@ -5,3 +5,6 @@ class Settings:
     @staticmethod
     def refresh_token_ttl() -> int:
         return int(current_app.config["JWT_REFRESH_TOKEN_EXPIRES"].total_seconds())
+
+    def black_list_ttl() -> int:
+        return int(current_app.config["JWT_ACCESS_TOKEN_EXPIRES"].total_seconds())
