8838453e912ebd0bc36e48cbaad5bfb41bc89ed2
Rafael Dantas <rafael.hfdantas@outlook.com>
2026-08-03 22:38:43 -0300

    refactor(frontend): change redirection route

--- frontend/src/routes/PublicRoute.jsx
+++ frontend/src/routes/PublicRoute.jsx
@@ -1,15 +1,16 @@
-import { useContext } from "react"
+import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext"
 import GlobalLoader from "../components/Loader/GlobalLoader";
+import { AuthContext } from "../context/AuthContext";
 
 export default function PublicRoute({ children }) {
+  const { user, loading } = useContext(AuthContext);
 
-    const { user, loading } = useContext(AuthContext);
+  if (loading) return <GlobalLoader message="Carregando..." />;
 
-    if (loading) return <GlobalLoader message="Carregando..." />;
-
-    if (user) return <Navigate to="/dashboard" replace />;
-
-    return children ? children : <Outlet />;
+  if (user) {
+    return <Navigate to="/" replace />;
   }
+
+  return children ? children : <Outlet />;
+}
